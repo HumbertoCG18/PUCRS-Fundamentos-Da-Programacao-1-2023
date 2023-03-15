e82c85a922bb7f29491f05ef52c2a194ca4a6b13
Humberto Corrêa <humberto.cgomes17@gmail.com>
2023-03-15 08:20:08 -0300

    Fixes

--- Aula 2 - 15.03.2023/errosDiversos/ProgramaComErroCompilacao01.java
+++ Aula 2 - 15.03.2023/errosDiversos/ProgramaComErroCompilacao01.java
@@ -2,17 +2,17 @@
  * verificando erros de exceucao e/ou compilacao
  * 
  * @author (Giraffa adpatado de Profa. Silvia Moraes) 
- * @version (teste )
+ * @version (1.0)
  */
 /**
  * Write a description of class ProgramaComErroCompilacao01 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Humberto Corrêa Gomes, Correção do arquivo da Professora Giraffa) 
+ * @version (1.1)
  */
 public class ProgramaComErroCompilacao01
 {
-    Public static void main(String args[]){
+    public static void main(String args[]){
         System.out.println("\fParabéns! Conseguiste corrigir o erro de sintaxe que é identificado em tempo de compilação. ");
     }
  
