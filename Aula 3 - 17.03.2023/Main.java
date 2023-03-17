c9d352c75d87755b8f7692cb354da6a3e13b7841
Humberto Corrêa <humberto.cgomes17@gmail.com>
2023-03-17 11:01:37 -0300

    Create Main.java

---
+++ Aula 3 - 17.03.2023/Main.java
@@ -0,0 +1,21 @@
+
+public class Main{
+    public static void main(String args[]){
+        System.out.println("\fDigite a medida do lado do quadrado");
+        double lado = teclado.nextDouble();
+        System.out.println("\fDigite a medida do raio");
+        double raio = teclado.nextDouble();
+        System.out.println("\fArea do Quadrado:" +areaQuadrado(lado));
+        System.out.println("\fArea do Circulo:" +areaCirculo(raio));
+    }
+
+    public static double areaQuadrado(double ladoQ){
+        return ladoq*lado;
+    }
+
+    public static double areaCirculo(double raio){
+    return Math.pi * raio * raio; 
+
+    }
+ 
+}
