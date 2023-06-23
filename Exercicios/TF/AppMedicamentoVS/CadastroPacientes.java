479c125b04a743df8de2d60df1946d602482aa0c
Humberto Corrêa <humberto.cgomes17@gmail.com>
2023-06-23 10:08:39 -0300

    update

---
+++ Exercicios/TF/AppMedicamentoVS/CadastroPacientes.java
@@ -0,0 +1,85 @@
+package Exercicios.TF.AppMedicamentoVS;
+import java.util.Arrays;
+
+public class CadastroPacientes {
+    private Paciente[] cadastro;
+    private int numeroDePacientes = 0;
+    
+    public CadastroPacientes() {
+        cadastro = new Paciente[10];
+    }
+    
+    public void addNoVetor(Paciente paciente) {
+        if (numeroDePacientes < 10) {
+            System.out.println("Paciente adicionado");
+            cadastro[numeroDePacientes] = paciente;
+            numeroDePacientes += 1;
+        } else {
+            System.out.println("Número máximo de pacientes atingido");
+        }
+    }
+    
+    public void removerNoVetor(Paciente paciente) {
+        for (int i = 0; i < cadastro.length; i++) {
+            if (cadastro[i] == paciente){
+                cadastro[i] = null;
+                
+                numeroDePacientes -= 1;
+            }
+        }
+    }
+
+    public void listarPacientes () {
+        for (int i = 0; i < cadastro.length; i++) {
+            System.out.println("Paciente " + (i + 1) + ":");
+            
+            System.out.println(cadastro[i]);
+            
+            System.out.println();
+        }
+    }
+    
+    public void listarPacientesRegistrados () {
+        for (int i = 0; i < cadastro.length; i++) {
+            if (cadastro[i] != null) {
+                System.out.println("Paciente " + (i + 1) + ":");
+                
+                System.out.println(cadastro[i]);
+                
+                System.out.println();
+            }
+        }
+    }
+    
+    public void listarPacientesAlfabetica() {
+        Paciente[] pacientesOrdenados = Arrays.copyOf(cadastro, numeroDePacientes);
+        Arrays.sort(pacientesOrdenados, (p1, p2) -> p1.getNome().compareToIgnoreCase(p2.getNome()));
+
+        System.out.println("---- Nomes em Ordem Crescente ----");
+        for (int i = 0; i < numeroDePacientes; i++) {
+            System.out.println(pacientesOrdenados[i].getNome());
+            
+            System.out.println();
+        }
+    }
+    
+    public void porcentagemDiagnosticos() {
+        int[] contadorDiagnosticos = new int[4];
+        String[] nomesDiagnosticos = {"Covid", "Zica", "Chikungunya", "Dengue"};
+
+        for (int i = 0; i < numeroDePacientes; i++) {
+            int diagnostico = cadastro[i].getDiagnostico();
+            contadorDiagnosticos[diagnostico - 1]++;
+        }
+
+        System.out.println("---- Percentagem de Pacientes por Diagnóstico ----");
+        for (int i = 0; i < contadorDiagnosticos.length; i++) {
+            double percentagem = (double) contadorDiagnosticos[i] / numeroDePacientes * 100;
+            System.out.println(nomesDiagnosticos[i] + ": " + String.format("%.2f", percentagem) + "%");
+        }
+    }
+    
+    public Paciente getPacienteFromIndex(int index) {
+        return cadastro[index];
+    }
+}
---
+++ Exercicios/TF/AppMedicamentoVS/Paciente.java
@@ -0,0 +1,81 @@
+package Exercicios.TF.AppMedicamentoVS;
+public class Paciente
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String nome, cpf, telefone;
+    private int diagnostico;
+    private Medicamento medicamento;
+
+    //Construtor para objetos da classe Paciente
+    public Paciente(String nome, String cpf, String telefone, int diagnostico, Medicamento medicamento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.diagnostico = diagnostico;
+        this.medicamento = medicamento;
+    }
+    
+    public String toString() {
+        String retorno = "Nome: " + nome + "\nCPF: " + cpf + "\nTelefone: " + telefone + "\nDiagnóstico: " + getDiagnosticoString();
+        
+        retorno += "\nMedicamento: " + (medicamento != null ? medicamento.getNome() : "Indisponível");
+        retorno += "\nQuantidade de caixas indicada pelo médico: " + medicamento.getQuantCaixas();
+        
+        return retorno;
+    }
+    
+    public String getDiagnosticoString() {
+        switch(diagnostico) {
+            case 1:
+                return "Covid";
+            case 2:
+                return "Zika";
+            case 3:
+                return "Chikungunya";
+            case 4:
+                return "Dengue";
+            default:
+                return "Indisponível";
+        }
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    public int getDiagnostico() {
+        return diagnostico;
+    }
+    
+    public Medicamento getMedicamento() {
+        return medicamento;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public void setDiagnostico(int diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+    
+    public void setMedicamento(Medicamento medicamento) {
+        this.medicamento = medicamento;
+    }
+}
